0c2f52044043885b76e6524a44599ea68dfed47e
Dylan Raga <dylanraga@gmail.com>
2022-08-04 15:15:32 -0700

    Package modularization & implemented Lab<->LCH conversions

--- modules/space/xyz.ts
+++ modules/space/xyz.ts
@@ -6,10 +6,21 @@ type OBSERVER_STRINGS = 'CIE 1931 2deg' | 'CIE 1964 10deg';
 
 class XYZSpace extends ColorSpace {
 	public name: string = 'XYZ ColorSpace';
-	public keys: string[] = ['X', 'Y', 'Z'];
+	public keys: string[] = ['X', 'Y', 'Z'];	
+	public static defaultSpace: XYZSpace;
 	
 	constructor(illuminant: xy, observer: OBSERVER_STRINGS) {
 		super();
+	}
+
+	public register(names: string[]): void;
+	public register(name: string): void;
+	public register(arg1: string | string[]): void {
+		const strings = typeof arg1 === 'string'? [arg1] : arg1;
+		
+		for (const name of strings) {
+			XYZSpace.named[name] = this;
+		}
 	}
 
 	CAT(illuminant: xy, method: any) {
@@ -19,5 +30,15 @@ class XYZSpace extends ColorSpace {
 
 //http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
 //TODO: add CAT
+
+export interface XYZSpaceNamedMap { }
+export type XYZSpaceName = keyof XYZSpaceNamedMap | (string & Record<never, never>);
+
+declare module '../space' {
+	interface ColorSpaceNamedMap extends XYZSpaceNamedMap {}
+}
+
+type XYZSpaceNamedMapType = XYZSpaceNamedMap & { [k: string]: XYZSpace };
+export const xyzSpaces = XYZSpace.named as XYZSpaceNamedMapType;
 
 export { XYZSpace };
